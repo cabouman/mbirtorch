2fb59d4004b623f8c0f875dae22fa0979797bb11
gbuzzard <buzzard@purdue.edu>
2026-08-07 09:10:07 -0400

    Update dev scripts and pyproject.toml.

--- dev_scripts/clean_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Editable install into the ACTIVE environment (mirror of mbirjax's dev_scripts
-# convention).  Run from the repo root or this directory.
-cd "$(dirname "$0")/.." || exit 1
-pip uninstall -y mbirtorch
-pip install -e ".[test]"
